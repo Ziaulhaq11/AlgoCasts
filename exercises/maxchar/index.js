9ca55d6c345c6fbe78506ec3db8356307902fd2d
Ziaulhaq <ziaullhaq11@gmail.com>
2021-07-18 22:06:41 +0530

    done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,20 +4,22 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
-let str = 'abbddddddd'
+let str = 'apple 1231111'
 let char = {}
 
 function maxChar(str) {
     let max = 1;
+    let maxnumber = 1;
     for(let i of str) {
         char[i] = char[i] + 1 || 1;
-        if (max < char[i]) {
+        if (maxnumber < char[i]) {
+            maxnumber = char[i]
             max = Object.keys(char).find(key => char[key] == char[i])
         }
     }
-    console.log(max);
+    return max;
 }
-maxChar(str)
+console.log(maxChar(str))
 
 //console.log(char)
 
